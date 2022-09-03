ef8534301e76f322802535ae50e7141457a922b8
BM-K <klbm126@khu.ac.kr>
2022-09-03 17:14:55 +0900

    Create 이진변환반복하기.py

---
+++ Programmers/이진변환반복하기.py
@@ -0,0 +1,9 @@
+def solution(s):
+    count_bin_change, count_del_zero = 0, 0
+    while s != '1':
+        count_del_zero += s.count('0')
+        s = s.replace('0', '')
+        s = format(len(s), 'b')
+        count_bin_change += 1
+
+    return [count_bin_change, count_del_zero]\
